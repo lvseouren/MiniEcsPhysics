805ba2299001e6ed49c8823898d495a6448e67d6
DiracseaFish <602633512@qq.com>
2023-12-28 16:40:41 +0800

    job test

--- Assets/Physics/IntegrateVelocitySystem.cs
+++ Assets/Physics/IntegrateVelocitySystem.cs
@@ -1,5 +1,10 @@
 using MiniEcs.Core;
 using MiniEcs.Core.Systems;
+using System.Collections.Generic;
+using Unity.Burst;
+using Unity.Collections;
+using Unity.Jobs;
+using UnityEngine.Jobs;
 using XFixMath.NET;
 
 namespace Physics
@@ -10,7 +15,8 @@ namespace Physics
     public class IntegrateVelocitySystem : IEcsSystem
     {
         private readonly EcsFilter _filter;
-
+        List<TransformComponent> transformComponents = new List<TransformComponent>();
+        List<XFix64Vector2> velocities = new List<XFix64Vector2>();
         public IntegrateVelocitySystem()
         {
             _filter = new EcsFilter().AllOf<TransformComponent, RigBodyComponent>().NoneOf<RigBodyStaticComponent>();
@@ -18,10 +24,47 @@ namespace Physics
 
         public void Update(XFix64 deltaTime, EcsWorld world)
         {
+            transformComponents.Clear();
+            velocities.Clear();
             world.Filter(_filter).ForEach((IEcsEntity entity, TransformComponent transform, RigBodyComponent rigBody) =>
             {
-                transform.Position += rigBody.Velocity * deltaTime;
+                transformComponents.Add(transform);
+                velocities.Add(rigBody.Velocity);
+                //transform.Position += rigBody.Velocity * deltaTime;
             });
+            NativeArray<XFix64Vector2> pos = new NativeArray<XFix64Vector2>(transformComponents.Count, Allocator.TempJob);
+            NativeArray<XFix64Vector2> vel = new NativeArray<XFix64Vector2>(transformComponents.Count, Allocator.TempJob);
+            for (int i = 0; i < transformComponents.Count; i++)
+            {
+                pos[i] = transformComponents[i].Position;
+                vel[i] = velocities[i];
+            }
+            var job = new TestJob2()
+            {
+                positions = pos,
+                velocities = vel,
+                deltaTime = deltaTime,
+            };
+            var jobHandler = job.Schedule(transformComponents.Count,64);
+            jobHandler.Complete();
+            for(int i = 0; i < transformComponents.Count; ++i)
+            {
+                transformComponents[i].Position = pos[i];
+            }
+        }
+
+        [BurstCompile]
+        public struct TestJob2 : IJobParallelFor
+        {
+            public NativeArray<XFix64Vector2> positions;
+            public NativeArray<XFix64Vector2> velocities;
+            public float deltaTime;
+
+            [BurstCompile]
+            public void Execute(int index)
+            {
+                positions[index] += velocities[index] * deltaTime;
+            }
         }
     }
 }
